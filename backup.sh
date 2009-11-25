67b21b3424f8b08603451f8da465ac604b2319e5
orkaa <orkica@gmail.com>
2009-11-25 18:33:31 +0100

    first commit

---
+++ backup.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+
+HELP="Use backup for normal backup\n
+full to force full backup\n
+rotate-view to view old sets\n 
+rotate-force to actually delete old sets\n
+status to view current backup sets status "
+######################################## CONFIG ################################################
+### The gpg key you want to use and a passphrase for it.
+#SIGNKEY=208D76D4
+#ENCKEY=$SIGNKEY
+#export PASSPHRASE=''
+
+# How old should the backups be before deleted. 
+AGE=2W
+
+# Where to backup. Can be local or remote path.
+TARGET="file:///mnt/backup"
+#TARGET="scp://user@hostname//mnt/backup"
+
+# Directories you want to include.
+DIRS="--include /etc  
+	--include /var"
+
+# Directories you want to exclude.
+EDIRS="--exclude /porn
+	--exclude /proc"
+
+# Additional ssh options
+#SSH_OPTIONS="--ssh-options=\"-oPort=1234\""
+
+# Encryption yes/no 
+ENCRYPTION="--no-encryption"
+#ENCRYPTION="--encrypt-key $ENCKEY --sign-key $SIGNKEY"
+################################################################################################
+
+#### LOCK FILE IMPLEMENTATION ####
+if [ -f /var/lock/duplicity ]; then
+	echo "Duplicity already running!"
+	exit
+else
+	touch /var/lock/duplicity
+fi
+
+case $1 in
+"backup")
+duplicity \
+   $ENCRYPTION \
+   $SSH_OPTIONS \
+   --volsize 100  \
+   $EDIRS \
+   $DIRS \
+   --exclude '**' / \
+   $TARGET
+;;
+
+"full")    
+duplicity full \
+   $ENCRYPTION \
+   $SSH_OPTIONS \
+   --volsize 100  \
+   $EDIRS \
+   $DIRS \
+   --exclude '**' / \
+   $TARGET
+;;
+
+"rotate-view") 
+duplicity remove-older-than $AGE \
+   $ENCRYPTION \
+   $SSH_OPTIONS \
+   $TARGET
+;;
+
+"rotate-force") 
+duplicity remove-older-than $AGE \
+   $ENCRYPTION \
+   $SSH_OPTIONS \
+    --force \
+   $TARGET
+;;
+
+"status")
+duplicity collection-status \
+   $ENCRYPTION \
+   $SSH_OPTIONS \
+    $TARGET
+;;
+
+"help")
+echo -e $HELP
+;;
+
+*)
+echo "Use help for usage tips"
+;;
+esac
+### clean up
+rm -f /var/lock/duplicity
