87b140b6b5b8add00c12240611b9ad30c0e164f7
orkaa <orkaa@ringlo.(none)>
2010-07-09 19:00:13 +0200

    some docs added

--- monitor.py
+++ monitor.py
@@ -1,6 +1,13 @@
+# This script is to be used with Zabbix monitoring. It writes "backup.log", each time it runs, which can be parsed with Zabbix. If everything is correct, log will contain this string: "Status: GOOD"
+
 import subprocess
 import datetime
 import sys
+
+##### CONFIG #####
+# How many days can pass between backups
+DELTA = 2
+##################
 
 months = {'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12 }
 
@@ -35,7 +42,7 @@ backup_time = datetime.datetime(int(line[5]), int(months[line[2]]), int(line[3])
 
 current_time = datetime.datetime.now()
 
-delta = datetime.timedelta(2)
+delta = datetime.timedelta(int(DELTA))
 
 if (current_time - backup_time) >= delta:
 	logfile.write('Backup did not complete in given time.\nLast good: %s\nStatus: BAD\n' % backup_time)
